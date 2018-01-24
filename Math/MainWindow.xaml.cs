fc9e51b58f40519c2dd3b792bdde3622089e31b1
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-01-24 16:20:39 +0200

    mats

--- Math/MainWindow.xaml.cs
+++ Math/MainWindow.xaml.cs
@@ -27,7 +27,12 @@ namespace Math
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            result.Text = (Int32.Parse(b1.Text) * Int32.Parse(b5.Text) * Int32.Parse(b9.Text)
+                + Int32.Parse(b2.Text) * Int32.Parse(b6.Text) * Int32.Parse(b7.Text)
+                + Int32.Parse(b3.Text) * Int32.Parse(b4.Text) * Int32.Parse(b8.Text)
+                - Int32.Parse(b1.Text) * Int32.Parse(b6.Text) * Int32.Parse(b8.Text)
+                - Int32.Parse(b2.Text) * Int32.Parse(b4.Text) * Int32.Parse(b9.Text)
+                 - Int32.Parse(b3.Text) * Int32.Parse(b5.Text) * Int32.Parse(b7.Text)).ToString();
         }
     }
 }
