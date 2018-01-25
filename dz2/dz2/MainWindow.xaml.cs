e044ef720c0d0e92b151be98b5f0fb61eda7bf56
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-01-25 15:32:30 +0200

    s

--- dz2/dz2/MainWindow.xaml.cs
+++ dz2/dz2/MainWindow.xaml.cs
@@ -15,14 +15,30 @@ using System.Windows.Shapes;
 
 namespace dz2
 {
+   
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] myColor = { "Navy", "Blue", "Aqua", "Teal", "Olive", "Green", "Lime", "Yellow", "Orange",
+            "Red", "Maroon", "Fuchsia", "Purple", "Black", "Silver", "Gray", "White" };
+        Brush[] myBrush ={ Brushes.Navy, Brushes.Blue, Brushes.Aqua, Brushes.Teal, Brushes.Olive, Brushes.Green, Brushes.Lime, Brushes.Yellow, Brushes.Orange,
+            Brushes.Red, Brushes.Maroon, Brushes.Fuchsia, Brushes.Purple, Brushes.Black, Brushes.Silver, Brushes.Gray, Brushes.White};
         public MainWindow()
         {
             InitializeComponent();
+            for (int i = 0; i < 17; i++)
+            {
+                Button MyControl = new Button();
+                
+                MyControl.Content = myColor[i];
+                MyControl.Margin = new Thickness(2, 2, 2, 2);
+
+
+                MyControl.Foreground = myBrush[i];
+                WARP.Children.Add(MyControl);
+            }
         }
     }
 }
