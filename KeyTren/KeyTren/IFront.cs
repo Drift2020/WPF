a67b48aa716c0c33c3761e50c77f72b8edb5adca
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-02-14 22:31:29 +0200

    interfase

---
+++ KeyTren/KeyTren/IFront.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyTren
+{
+    public interface IFront
+    {
+         string prow_str { set; get; }
+         string my_str { set; get; }
+
+        string my_chars { set; get; }
+        bool sensitive { set; get; }
+
+        int fails { set; get; }
+        int speed_chars { set; get; }
+
+       
+
+        event EventHandler<EventArgs> DownKey;
+        event EventHandler<EventArgs> Start;
+        event EventHandler<EventArgs> Stop;
+
+        event EventHandler<EventArgs> Speed_Chars;
+        event EventHandler<EventArgs> Fails;
+    }
+}
--- KeyTren/KeyTren/MainWindow.xaml.cs
+++ KeyTren/KeyTren/MainWindow.xaml.cs
@@ -17,23 +17,71 @@ namespace KeyTren
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
+    struct mycolor
+    {
+        public Brush startBrush;
+        public Button temp;
+      
+    }
     public partial class MainWindow : Window
     {
+        List<mycolor> startBrush;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Grid_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            e.Handled = true;
+            startBrush = new List<mycolor>();
         }
 
         private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            MessageBox.Show(e.Key.ToString());
-            Button temp= (Button)Keys.FindName(e.Key.ToString());
-            temp.IsPressed = true;
+            
+            Button temp =   Keys.FindName(e.Key.ToString()) as Button;
+
+            if (e.Key.ToString()=="System")
+                temp = Keys.FindName("Alt") as Button;
+           
+            if (temp != null )
+            {
+                for (int i = 0; i < startBrush.Count; i++)
+                {
+                    if (startBrush[i].temp.Content == temp.Content)
+                        return;
+                }
+                mycolor tempM = new mycolor();
+
+                tempM.temp = temp;
+                tempM.startBrush = temp.Background;
+                startBrush.Add(tempM);
+
+                temp.Background = Brushes.IndianRed;
+            }
+        }
+
+        private void Window_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            Button temp = Keys.FindName(e.Key.ToString()) as Button;
+            if (e.Key.ToString() == "System")
+                temp = Keys.FindName("Alt") as Button;
+
+            if (temp != null)
+            {
+                for (int i = 0; i < startBrush.Count; i++)
+                {
+                    if (startBrush[i].temp.Content == temp.Content)
+                    {
+                        temp.Background = startBrush[i].startBrush;
+                        startBrush.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+
+           
+        }
+
+        private void Keys_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
