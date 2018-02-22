1f844e37c3b3a2c27123e86ea04493091b1a4f50
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-02-22 10:55:53 +0200

    s

--- BookFood/BookFood/Models/Food.cs
+++ BookFood/BookFood/Models/Food.cs
@@ -8,5 +8,17 @@ namespace BookFood.Models
 {
     class Food
     {
+        public Food()
+        {
+
+        }
+        public Food( string Name_food)
+        {
+
+        }
+        public string name_food;
+        public string image_path;
+        public List<string> list_ingridient;
+        public string info_food;
     }
 }
