a63cf18444398efdc7ca2b6e2d1e40b5321368dc
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-03-08 19:46:04 +0200

    e

--- Gallery/Gallery/Indecs.xaml.cs
+++ Gallery/Gallery/Indecs.xaml.cs
@@ -22,6 +22,23 @@ namespace Gallery
         public Indecs()
         {
             InitializeComponent();
+            DefaultStyle();
+        }
+        public void DefaultStyle()
+        {
+            this.Background = new SolidColorBrush(Colors.DarkGray);
+            var core = new Uri("Core.xaml", UriKind.Relative);
+            var brush = new Uri("Brushes.xaml", UriKind.Relative);
+            var ico = new Uri("Icons.xaml", UriKind.Relative);
+
+            ResourceDictionary resourceDictionary = Application.LoadComponent(core) as ResourceDictionary;
+            ResourceDictionary bru = Application.LoadComponent(brush) as ResourceDictionary;
+            ResourceDictionary ic = Application.LoadComponent(ico) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            Application.Current.Resources.MergedDictionaries.Add(bru);
+            Application.Current.Resources.MergedDictionaries.Add(ic);
+
         }
     }
 }
