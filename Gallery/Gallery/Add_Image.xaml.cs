854908117fb3cb6826dea46fb4cd9d2390b56b15
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-03-08 03:04:16 +0200

    s

---
+++ Gallery/Gallery/Add_Image.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gallery
+{
+    /// <summary>
+    /// Interaction logic for Add_Image.xaml
+    /// </summary>
+    public partial class Add_Image : Window
+    {
+        public Add_Image()
+        {
+            InitializeComponent();
+        }
+    }
+}
